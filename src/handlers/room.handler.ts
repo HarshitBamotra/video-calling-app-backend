9d5e17fd6349973ddd630e57a68d46e62a2822dc
Harshit Bamotra <harshitbamotra.01@gmail.com>
2025-02-27 12:46:20 +0530

    Room creation logic

--- src/handlers/room.handler.ts
+++ src/handlers/room.handler.ts
@@ -1,22 +1,31 @@
 import { Socket } from "socket.io/dist";
 import {v4 as UUIDv4} from "uuid";
+import IRoomParams from "../interfaces/i.room.params";
 
+const rooms: Record<string, string[]> = {};
 const roomHandler = (socket: Socket) => {
 
-    const createRoom = ()=>{
+    const createRoom = () => {
         const roomId = UUIDv4();
 
         socket.join(roomId);
+        rooms[roomId] = [];
         socket.emit("room-created", {roomId});
         console.log(`room created with id: ${roomId}`);
-    }
+    };
 
-    const joinRoom = ({roomId}: {roomId: String})=>{
-        console.log("Room joined: ", roomId);
-    }
+    const joinedRoom = ({roomId, peerId}: IRoomParams) => {
+        if (rooms[roomId]) {
+            console.log("Room joined: ", roomId, "with peer id: ", peerId);
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+
+            socket.emit("get-users", {participants: rooms[roomId]});
+        }
+    };
 
     socket.on("create-room", createRoom);
-    socket.on("joined-room", joinRoom);
-}
+    socket.on("joined-room", joinedRoom);
+};
 
 export default roomHandler;
